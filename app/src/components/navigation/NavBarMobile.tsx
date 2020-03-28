b56e5b891a2533ecc7356e712c90715ce92c0252
Krin <juangbhanich.k@gmail.com>
2020-03-28 17:22:30 +1100

    Mobile Nav

--- app/src/components/navigation/NavBarMobile.tsx
+++ app/src/components/navigation/NavBarMobile.tsx
@@ -1,12 +1,33 @@
 import React from 'react';
 import NavigationModel from './NavigationModel';
 import BlockTheme from '../../utils/structure/ContentBlock/BlockTheme';
+import "../../style/navigation.scss";
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+
+const createMobileNavLinkElement = (label: string, link: string) => {
+    return <div className="mobile-nav-element"><Link className="mobile-nav-link" to={link}>
+        <FontAwesomeIcon icon="bars" style={{marginRight: "1rem"}}/>{label}
+    </Link></div>;
+}
 
 
 const NavBarMobile: React.FC<NavigationModel> = (props) => {
+
     const theme: string = BlockTheme.BASIC;
+    const navTitle = <div className="mobile-nav-title"><div className={theme}><h1>Menu</h1></div></div>
+
+    const navElements: JSX.Element[] = [];
+    for (let i: number = 0; i < props.links.length; i ++) {
+        const navLink = props.links[i];
+        const navElement = createMobileNavLinkElement(navLink.label, navLink.link);
+        navElements.push(navElement);
+    }
+
     return <div className={`mobile-nav ${theme}`}>
-        New NavBarMobile
+        {navTitle}
+        {navElements}
     </div>;
 }
 
